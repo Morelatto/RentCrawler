eb9361fc401f5c1e90fbf0a963590da588d76867
morelatto <pmichilis@uolinc.com>
2018-08-25 19:51:24 -0300

    Added page crawl. Added run configuration.

--- vr_crawler/spiders/vivareal.py
+++ vr_crawler/spiders/vivareal.py
@@ -1,18 +1,14 @@
 import scrapy
-from scrapy import Request
 
 from vr_crawler.items import ApartmentLoader
 
 
 class QuotesSpider(scrapy.Spider):
     name = 'vivareal'
-    start_urls = ['https://www.vivareal.com.br/aluguel/sp/sao-paulo/apartamento_residencial/']
+    start_url = 'https://www.vivareal.com.br/aluguel/sp/sao-paulo/apartamento_residencial/'
 
     def start_requests(self):
-        requests = []
-        for url in self.start_urls:
-            requests.append(Request(url=url, headers={'Referer': 'https://www.vivareal.com.br'}))
-        return requests
+        return [scrapy.Request(url=self.start_url, headers={'Referer': 'https://www.vivareal.com.br'})]
 
     def parse(self, response):
         for res in response.css('.results-list article'):
@@ -20,14 +16,19 @@ class QuotesSpider(scrapy.Spider):
             loader.add_css('name', 'h2 a::text')
             loader.add_css('address', 'h2 span::text')
 
-            # details = res.css('ul.property-card__details')
-            # loader.add_css('size', details.css('li.property-card__detail-area span::text').extract())
-            # loader.add_css('rooms', details.css('li.js-property-detail-rooms span::text').extract())
-            # loader.add_css('bathrooms', details.css('li.js-property-detail-bathroom" span::text').extract())
-            # loader.add_css('garages', details.css('li.js-property-detail-garages" span::text').extract())
+            details_loader = loader.nested_css('ul.property-card__details')
+            details_loader.add_css('size', 'li.property-card__detail-area span::text')
+            details_loader.add_css('rooms', 'li.js-property-detail-rooms span::text')
+            details_loader.add_css('bathrooms', 'li.js-property-detail-bathroom span::text')
+            details_loader.add_css('garages', 'li.js-property-detail-garages span::text')
 
             loader.add_css('rent', 'div.js-property-card__price-small::text')
             loader.add_css('condo', 'strong.js-condo-price::text')
 
             loader.add_css('description', 'div.js-property-description::text')
-            return loader.load_item()
+            yield loader.load_item()
+
+        next_page = response.css('a.js-change-page::attr(data-page)')[-1].extract()
+        if next_page:
+            next_page = '{url}?pagina={page}'.format(url=self.start_url, page=next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
