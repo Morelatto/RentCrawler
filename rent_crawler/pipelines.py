442b2302b0eb34c021b9c4ea6f67f085a19ab7f3
Morelatto <pmichilis@uolinc.com>
2019-07-13 19:35:18 -0300

    Replaced sqlite pipeline by mongo and cleaned up settings.

--- rent_crawler/pipelines.py
+++ rent_crawler/pipelines.py
@@ -12,52 +12,6 @@ DISTRICTS_TO_DOWNLOAD = ['Jardim Paulistano', 'Pinheiros', 'Jardim Europa', 'Jar
                          'Cerqueira César', 'Jardim Paulista', 'Bela Vista', 'Consolação', 'Paraíso', 'Vila Mariana',
                          'Chácara Klabin', 'Higienópolis', 'Aclimação']
 
-logger = logging.getLogger(__name__)
-logger.setLevel('DEBUG')
-
-
-class ApartmentPipeline:
-    def __init__(self):
-        self.db = r'apartments.sqlite'
-        self.table = 'apartments'
-        self.connection = sqlite3.connect(self.db)
-        self.cursor = self.connection.cursor()
-        self.__create_table()
-
-    def process_item(self, item, _):
-        self.__upsert(item)
-        self.connection.commit()
-        return item
-
-    def __upsert(self, item):
-        item_address = item['address']
-        item_prices = item['prices']
-        item_details = item['details']
-        item_text_details = item['text_details']
-        values = (item_address.get('street'), item_address.get('district'), item_address.get('city'),
-                  item_details.get('size'), item_details.get('rooms'), item_details.get('suite'),
-                  item_details.get('bathrooms'), item_details.get('garages'), item_prices.get('rent'),
-                  item_prices.get('condo'), item_prices.get('iptu'), item_text_details.get('characteristics'),
-                  item_text_details.get('description'), item.get('source'), item.get('code'))
-        try:
-            self.cursor.execute("INSERT INTO {} VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);".format(self.table), values)
-        except sqlite3.IntegrityError:
-            logger.debug('Updating item already on db: ' + item.get('code'))
-            self.cursor.execute("UPDATE {} SET street = ?, district = ?, city = ?, size = ?, rooms = ?, suite = ?,"
-                                " bathrooms = ?, garages = ?, rent = ?, condo = ?, iptu=?, characteristics=?, "
-                                "description = ?, source = ? WHERE code = ?;".format(self.table), values)
-
-    def __create_table(self):
-        self.cursor.execute(
-            "CREATE TABLE IF NOT EXISTS {} (street TEXT, district TEXT, city TEXT, size INT, rooms INT, suite INT, "
-            "bathrooms INT, garages INT, rent INT, condo INT, iptu INT, characteristics TEXT, description TEXT, "
-            "source TEXT, code INT PRIMARY KEY)".format(self.table))
-        self.connection.commit()
-
-    def __del__(self):
-        self.cursor.close()
-        self.connection.close()
-
 
 # TODO debug with zap
 class ApartmentPicturesPipeline(ImagesPipeline):
--- rent_crawler/settings.py
+++ rent_crawler/settings.py
@@ -5,37 +5,33 @@ BOT_NAME = 'rent_crawler'
 SPIDER_MODULES = ['rent_crawler.spiders']
 NEWSPIDER_MODULE = 'rent_crawler.spiders'
 
-USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-
-ROBOTSTXT_OBEY = False
+USER_AGENT = \
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
 
 # CONCURRENT_REQUESTS = 32
 
-# DOWNLOAD_DELAY = 0.5
-# The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
-# CONCURRENT_REQUESTS_PER_IP = 16
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_DEBUG = True
+
+DOWNLOAD_DELAY = 1
 
 TELNETCONSOLE_ENABLED = False
+EXTENSIONS = {
+    'scrapy.extensions.telnet.TelnetConsole': None,
+}
 
 ITEM_PIPELINES = {
-    'rent_crawler.pipelines.ApartmentPipeline': 300,
+    'scrapy_mongodb.MongoDBPipeline': 300,
     'rent_crawler.pipelines.ApartmentPicturesPipeline': 1
 }
 
-IMAGES_STORE = 'pictures/'
-
-# AUTOTHROTTLE_ENABLED = True
-# AUTOTHROTTLE_START_DELAY = 5
-# AUTOTHROTTLE_MAX_DELAY = 60
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# AUTOTHROTTLE_DEBUG = False
-
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = 'httpcache'
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+MONGODB_DATABASE = 'rent'
+MONGODB_COLLECTION = 'places'
+MONGODB_UNIQUE_KEY = 'code'
 
 # LOG_STDOUT = True
 # LOG_FILE = 'spider_log.txt'
+
+IMAGES_STORE = 'pictures/'
+
+CLOSESPIDER_ITEMCOUNT = 100
