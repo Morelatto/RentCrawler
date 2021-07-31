6fb967bd37062fd1a7bdbc82ec4e9c19457e6664
Morelatto <morelatto@pm.me>
2021-07-31 17:35:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='1.0',
     packages=find_packages(),
     entry_points={'scrapy': ['settings = rent_crawler.settings']},
-    install_requires=['scrapy_dynamodb==0.2.3']
+    install_requires=['scrapy-mongodb==0.12.0', 'redis==3.5.3', 'ScrapyElasticSearch==0.9.2']
 )
