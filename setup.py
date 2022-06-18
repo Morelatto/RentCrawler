2d2d24bd50a6ecc7bde073e9c671287d14e422f5
Pedro Morelatto <morelatto@pm.me>
2022-06-18 14:17:09 -0300

    Removes unused files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='RentCrawler',
-    version='1.0',
-    packages=find_packages(),
-    entry_points={'scrapy': ['settings = rent_crawler.settings']},
-    install_requires=['scrapy-mongodb==0.12.0', 'redis==3.5.3', 'ScrapyElasticSearch==0.9.2']
-)
