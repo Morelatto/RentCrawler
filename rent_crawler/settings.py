4166b5739976fbe51daefce52f3ad504cc43a48c
Morelatto <morelatto@pm.me>
2023-08-06 19:55:25 -0300

    chore(settings.py): increase CONCURRENT_REQUESTS_PER_DOMAIN to 2 for improved crawling performance chore(settings.py): decrease AUTOTHROTTLE_START_DELAY to 2.5 and AUTOTHROTTLE_MAX_DELAY to 10 for faster crawling chore(settings.py): enable SCRAPY_POET_PROVIDERS with "rent_crawler.providers.BodyJsonProvider" at priority 500 for better scraping chore(settings.py): comment out unused DOWNLOADER_MIDDLEWARES for cleaner code chore(settings.py): enable SCHEDULER, DUPEFILTER_CLASS, and DUPEFILTER_DEBUG for using Scrapy-Redis's scheduler and dupefilter chore(settings.py): set SCHEDULER_PERSIST to True to allow pause/resume crawls without cleaning up Redis queues chore(settings.py): set SCHEDULER_QUEUE_CLASS to 'scrapy_redis.queue.SpiderPriorityQueue' for scheduling requests using a priority queue chore(settings.py): remove unused ITEM_PIPELINES and keep only "rent_crawler.pipelines.MongoDBPipeline" at priority 100 chore(settings.py): set LOG_FORMATTER to "rent_crawler.loggers.SpiderLogFormatter" and LOG_LEVEL to 'DEBUG' for better logging chore(settings.py): remove unused comments and variables for cleaner code

--- rent_crawler/settings.py
+++ rent_crawler/settings.py
@@ -4,45 +4,52 @@ BOT_NAME = "rent_crawler"
 SPIDER_MODULES = ["rent_crawler.spiders"]
 NEWSPIDER_MODULE = "rent_crawler.spiders"
 
-DOWNLOAD_DELAY = 3
-DOWNLOAD_TIMEOUT = 16
-
-CONCURRENT_REQUESTS_PER_DOMAIN = 1
+CONCURRENT_REQUESTS_PER_DOMAIN = 2
 
 AUTOTHROTTLE_ENABLED = True
-AUTOTHROTTLE_START_DELAY = 5
-AUTOTHROTTLE_MAX_DELAY = 15
+AUTOTHROTTLE_START_DELAY = 2.5
+AUTOTHROTTLE_MAX_DELAY = 10
 AUTOTHROTTLE_DEBUG = True
 
 COOKIES_ENABLED = False
 TELNETCONSOLE_ENABLED = False
 
-DOWNLOADER_MIDDLEWARES = {
-    "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
-    "scrapy.downloadermiddlewares.retry.RetryMiddleware": None,
-    "scrapy_fake_useragent.middleware.RandomUserAgentMiddleware": 400,
-    "scrapy_fake_useragent.middleware.RetryUserAgentMiddleware": 401,
-    'scrapy_poet.InjectionMiddleware': 543,
+SCRAPY_POET_PROVIDERS = {
+    "rent_crawler.providers.BodyJsonProvider": 500,
 }
 
-DUPEFILTER_CLASS = 'scrapy.dupefilters.BaseDupeFilter'
-
-FAKEUSERAGENT_PROVIDERS = [
-    "scrapy_fake_useragent.providers.FakeUserAgentProvider",
-    "scrapy_fake_useragent.providers.FakerProvider",
-    "scrapy_fake_useragent.providers.FixedUserAgentProvider",
-]
+DOWNLOADER_MIDDLEWARES = {
+    # "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
+    # "scrapy.downloadermiddlewares.retry.RetryMiddleware": None,
+    # "scrapy_fake_useragent.middleware.RandomUserAgentMiddleware": 400,
+    # "scrapy_fake_useragent.middleware.RetryUserAgentMiddleware": 401,
+    'scrapy_poet.InjectionMiddleware': 543,
+}
 
 USER_AGENT = (
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 "
     "Safari/601.3.9"
 )
 
+# FAKEUSERAGENT_PROVIDERS = [
+#     "scrapy_fake_useragent.providers.FakeUserAgentProvider",
+#     "scrapy_fake_useragent.providers.FakerProvider",
+#     "scrapy_fake_useragent.providers.FixedUserAgentProvider",
+# ]
+
+# Use Scrapy-Redis's scheduler and dupefilter
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+DUPEFILTER_CLASS = "rent_crawler.dupefilter.RedisDupeFilter"
+DUPEFILTER_DEBUG = True
+
+# Don't cleanup Redis queues, allows pause/resume crawls
+SCHEDULER_PERSIST = True
+
+# Schedule requests using a priority queue (FIFO by default)
+SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.SpiderPriorityQueue'
+
 ITEM_PIPELINES = {
-    "rent_crawler.pipelines.RentCrawlerPipeline": 100,
-    "rent_crawler.pipelines.RedisDuplicatePipeline": 200,
-    "rent_crawler.pipelines.MongoDBPipeline": 300,
-    "rent_crawler.pipelines.RedisPipeline": 400,
+    "rent_crawler.pipelines.MongoDBPipeline": 100
 }
 
 MONGODB_URI = 'mongodb://root:pass@localhost:27017'
@@ -53,18 +60,15 @@ MONGODB_SEPARATE_COLLECTIONS = True
 
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
-REDIS_DB_NUMBER = 0
+#REDIS_PASSWORD = "root"
 
-# LOG_STDOUT = True
+LOG_FORMATTER = "rent_crawler.loggers.SpiderLogFormatter"
 # LOG_FILE = 'spider_log.txt'
-LOG_FORMATTER = "rent_crawler.loggers.QuietLogFormatter"
+# LOG_STDOUT = True
+LOG_LEVEL = 'DEBUG'
 
 # HTTPERROR_ALLOW_ALL = True
 
 REQUEST_FINGERPRINTER_IMPLEMENTATION = '2.7'
-
-SCRAPY_POET_PROVIDERS = {
-    "rent_crawler.providers.BodyJsonProvider": 500,
-}
 
 FEED_EXPORT_ENCODING = 'utf-8'
