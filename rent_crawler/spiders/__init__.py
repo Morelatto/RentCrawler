c5961bd66776f5ccc4d61ba9571fd5802fb55386
Morelatto <morelatto@pm.me>
2023-09-15 00:38:20 -0300

    refactor(spider): simplify spider code by using VRZapListPage class to convert page response to item
    feat(spider): pass VRZapListPage instance to parse method to improve code readability and maintainability

--- rent_crawler/spiders/__init__.py
+++ rent_crawler/spiders/__init__.py
@@ -1,86 +1,15 @@
 import scrapy
-from scrapy.loader import ItemLoader
 
-from rent_crawler.items import RentalPropertyLoader, AddressLoader, PricesLoader, DetailsLoader, TextDetailsLoader
-from rent_crawler.items import VRZapRentalProperty, VRZapAddress, VRZapPrices, VRZapDetails, VRZapTextDetails, \
-    VRZapMediaDetails
+from rent_crawler.items import VRZapProperty
+from rent_crawler.pages import VRZapListPage
 
 
 class BaseVrZapSpider(scrapy.Spider):
-    def __init__(self, start_page=1, pages_to_crawl=1, *args, **kwargs):
+    def __init__(self, start=1, pages=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.start_page = int(start_page)
-        self.pages_to_crawl = int(pages_to_crawl)
+        self.start_page = int(start)
+        self.pages_to_crawl = int(pages)
 
-    def parse(self, response, **kwargs) -> VRZapRentalProperty:
+    def parse(self, response: scrapy.http.Response, page: VRZapListPage, **kwargs) -> VRZapProperty:
         self.logger.info('Scraping page %d/%d', kwargs['page_number'], kwargs['total_pages'])
-
-        json_response = response.json()
-        self.logger.info(json_response['search'])
-        for result in json_response['search']['result']['listings']:
-            listing = result['listing']
-            loader = RentalPropertyLoader(item=VRZapRentalProperty())
-            loader.add_value('code', listing['id'])
-            loader.add_value('address', self.get_address(listing['address']))
-            loader.add_value('prices', self.get_prices(listing['pricingInfos']))
-            loader.add_value('details', self.get_details(listing))
-            loader.add_value('text_details', self.get_text_details(listing))
-            loader.add_value('media', self.get_media_details(result['medias']))
-            loader.add_value('url', result['link']['href'])
-            item = loader.load_item()
-            self.logger.info(item)
-            yield item
-
-    @classmethod
-    def get_address(cls, json_address: dict) -> VRZapAddress:
-        address_loader = AddressLoader(item=VRZapAddress())
-        address_loader.add_value('street', json_address.get('street'))
-        address_loader.add_value('street', json_address.get('streetNumber'))
-        address_loader.add_value('district', json_address.get('neighborhood'))
-        address_loader.add_value('city', json_address.get('city'))
-        address_loader.add_value('complement', json_address.get('complement'))
-        address_loader.add_value('cep', json_address.get('zipCode'))
-        address_loader.add_value('zone', json_address.get('zone'))
-        address_loader.add_value('location', json_address.get('point'))
-        return address_loader.load_item()
-
-    @classmethod
-    def get_prices(cls, json_prices: list) -> VRZapPrices:
-        for json_price in json_prices:
-            if json_price.get('businessType') == 'RENTAL':
-                prices_loader = PricesLoader(item=VRZapPrices())
-                prices_loader.add_value('rent', json_price.get('price'))
-                prices_loader.add_value('condo', json_price.get('monthlyCondoFee'))
-                prices_loader.add_value('iptu', json_price.get('yearlyIptu'))
-                prices_loader.add_value('total', json_price.get('price'))
-                prices_loader.add_value('total', json_price.get('monthlyCondoFee'))
-                prices_loader.add_value('total', json_price.get('yearlyIptu'))
-                return prices_loader.load_item()
-
-    @classmethod
-    def get_details(cls, json_listing: dict) -> VRZapDetails:
-        details_loader = DetailsLoader(item=VRZapDetails())
-        details_loader.add_value('area', json_listing.get('totalAreas'))
-        details_loader.add_value('area', json_listing.get('usableAreas'))
-        details_loader.add_value('bedrooms', json_listing.get('bedrooms'))
-        details_loader.add_value('suites', json_listing.get('suites'))
-        details_loader.add_value('bathrooms', json_listing.get('bathrooms'))
-        details_loader.add_value('parking', json_listing.get('parkingSpaces'))
-        return details_loader.load_item()
-
-    @classmethod
-    def get_text_details(cls, json_listing: dict) -> VRZapTextDetails:
-        text_details_loader = TextDetailsLoader(item=VRZapTextDetails())
-        text_details_loader.add_value('description', json_listing.get('description'))
-        text_details_loader.add_value('characteristics', json_listing.get('amenities'))
-        text_details_loader.add_value('title', json_listing.get('title'))
-        text_details_loader.add_value('contact', json_listing.get('advertiserContact').get('phones'))
-        text_details_loader.add_value('type', json_listing['unitTypes'])
-        return text_details_loader.load_item()
-
-    @classmethod
-    def get_media_details(cls, json_medias: list) -> VRZapMediaDetails:
-        media_details_loader = ItemLoader(item=VRZapMediaDetails())
-        media_details_loader.add_value('images', json_medias)
-        media_details_loader.add_value('video', json_medias)
-        return media_details_loader.load_item()
+        yield from page.to_item()
