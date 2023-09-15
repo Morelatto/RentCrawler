48a3e12325b26ac5e347032c547d9a6711ed1652
Morelatto <morelatto@pm.me>
2023-09-15 01:14:01 -0300

    refactor(zap.py): improve readability by adding a newline after import statements
    chore(zap.py): import urlencode from urllib.parse to fix missing import error
    
    fix(zap.py): remove unused query parameters from start_url to simplify the URL
    feat(zap.py): update start_requests method to use a dictionary for query parameters and dynamically generate the URL
    feat(zap.py): add logger info message to indicate the start of the crawl
    feat(zap.py): update start_requests method to use cb_kwargs to pass additional arguments to the callback function
    
    chore: remove unnecessary commented code and unused query parameters
    
    The commented code and unused query parameters were removed to improve code readability and maintainability.

--- rent_crawler/spiders/zap.py
+++ rent_crawler/spiders/zap.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+
 import scrapy
 
 from rent_crawler.spiders import BaseVrZapSpider
@@ -7,42 +9,52 @@ PAGE_SIZE = 24
 
 class ZapSpider(BaseVrZapSpider):
     name = 'zap'
-    start_url = 'https://glue-api.zapimoveis.com.br/v2/listings?' \
-                'unitSubTypes=UnitSubType_NONE,DUPLEX,LOFT,STUDIO,TRIPLEX|UnitSubType_NONE,SINGLE_STOREY_HOUSE,VILLAGE_HOUSE,KITNET|CONDOMINIUM|UnitSubType_NONE|PENTHOUSE|FLAT|KITNET|UnitSubType_NONE,CONDOMINIUM,VILLAGE_HOUSE|TWO_STORY_HOUSE|UnitSubType_NONE|UnitSubType_NONE,CONDOMINIUM' \
-                '&unitTypes=APARTMENT,HOME,HOME,COUNTRY_HOUSE,APARTMENT,APARTMENT,APARTMENT,ALLOTMENT_LAND,HOME,RESIDENTIAL_BUILDING,FARM' \
-                '&usageTypes=RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL' \
-                '&unitTypesV3=APARTMENT,HOME,CONDOMINIUM,COUNTRY_HOUSE,PENTHOUSE,FLAT,KITNET,RESIDENTIAL_ALLOTMENT_LAND,TWO_STORY_HOUSE,RESIDENTIAL_BUILDING,FARM' \
-                '&text=Apartamento' \
-                '&business=RENTAL' \
-                '&categoryPage=RESULT' \
-                '&parentId=null' \
-                '&listingType=USED' \
-                '&portal=ZAP' \
-                '&size={size}' \
-                '&from={from_}' \
-                '&page={page}' \
-                '&includeFields=search%28result%28listings%28listing%28displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status%29,account%28id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite%29,medias,accountLink,link%29%29,totalCount%29,expansion%28search%28result%28listings%28listing%28displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status%29,account%28id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite%29,medias,accountLink,link%29%29,totalCount%29%29,nearby%28search%28result%28listings%28listing%28displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status%29,account%28id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite%29,medias,accountLink,link%29%29,totalCount%29%29,page,fullUriFragments,developments%28search%28result%28listings%28listing%28displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status%29,account%28id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite%29,medias,accountLink,link%29%29,totalCount%29%29,superPremium%28search%28result%28listings%28listing%28displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status%29,account%28id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite%29,medias,accountLink,link%29%29,totalCount%29%29,owners%28search%28result%28listings%28listing%28displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status%29,account%28id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite%29,medias,accountLink,link%29%29,totalCount%29%29' \
-                '&cityWiseStreet=1' \
-                '&developmentsSize=3' \
-                '&superPremiumSize=3' \
-                '&addressCountry=' \
-                '&addressState=S%C3%A3o+Paulo' \
-                '&addressCity=S%C3%A3o+Paulo' \
-                '&addressZone=' \
-                '&addressNeighborhood=' \
-                '&addressStreet=' \
-                '&addressAccounts=' \
-                '&addressType=city' \
-                '&addressLocationId=BR%3ESao+Paulo%3ENULL%3ESao+Paulo' \
-                '&addressPointLat=-23.55052' \
-                '&addressPointLon=-46.633309'
+    start_url = 'https://glue-api.zapimoveis.com.br/v2/listings'
+
     headers = {
         'x-domain': 'www.zapimoveis.com.br'
     }
 
     def start_requests(self):
-        page = self.start_page
-        while page < self.start_page + self.pages_to_crawl:
-            req_url = self.start_url.format(size=PAGE_SIZE, from_=(page - 1) * PAGE_SIZE, page=page)
-            yield scrapy.Request(url=req_url, headers=self.headers)
-            page += 1
+        self.logger.info('Starting crawl of %d pages', self.pages_to_crawl)
+
+        for page in range(self.start_page, self.start_page + self.pages_to_crawl):
+            ZAP_DATA['from'] *= page
+
+            yield scrapy.Request(
+                url=f'{self.start_url}?{urlencode(ZAP_DATA)}',
+                headers=self.headers,
+                dont_filter=True,
+                cb_kwargs=dict(page_number=page, total_pages=self.pages_to_crawl)
+            )
+
+
+ZAP_DATA = {
+    'addressState': 'São Paulo',
+    'addressCity': 'São Paulo',
+    'addressZone': '',
+    'addressNeighborhood': '',
+    'addressStreet': '',
+    'addressLocationId': 'BR>Sao Paulo>NULL>Sao Paulo',
+    'addressPointLat': '-23.55052',
+    'addressPointLon': '-46.633309',
+    'text': 'Apartamento',
+    'business': 'RENTAL',
+    'categoryPage': 'RESULT',
+    'listingType': 'USED',
+    'portal': 'ZAP',
+    'size': PAGE_SIZE,
+    'from': PAGE_SIZE,
+    'usageTypes': 'RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL,RESIDENTIAL',
+    # 'unitTypes': 'APARTMENT,HOME,HOME,COUNTRY_HOUSE,APARTMENT,APARTMENT,APARTMENT,ALLOTMENT_LAND,HOME,RESIDENTIAL_BUILDING,FARM',
+    # 'unitTypesV3': 'APARTMENT,HOME,CONDOMINIUM,COUNTRY_HOUSE,PENTHOUSE,FLAT,KITNET,RESIDENTIAL_ALLOTMENT_LAND,TWO_STORY_HOUSE,RESIDENTIAL_BUILDING,FARM',
+    # 'unitSubTypes': 'UnitSubType_NONE,DUPLEX,LOFT,STUDIO,TRIPLEX|UnitSubType_NONE,SINGLE_STOREY_HOUSE,VILLAGE_HOUSE,KITNET|CONDOMINIUM|UnitSubType_NONE|PENTHOUSE|FLAT|KITNET|UnitSubType_NONE,CONDOMINIUM,VILLAGE_HOUSE|TWO_STORY_HOUSE|UnitSubType_NONE|UnitSubType_NONE,CONDOMINIUM',
+    # 'includeFields': 'search(result(listings(listing(displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite),medias,accountLink,link)),totalCount),expansion(search(result(listings(listing(displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite),medias,accountLink,link)),totalCount)),nearby(search(result(listings(listing(displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite),medias,accountLink,link)),totalCount)),page,fullUriFragments,developments(search(result(listings(listing(displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite),medias,accountLink,link)),totalCount)),superPremium(search(result(listings(listing(displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite),medias,accountLink,link)),totalCount)),owners(search(result(listings(listing(displayAddressType,amenities,usableAreas,constructionStatus,listingType,description,title,stamps,createdAt,floors,unitTypes,nonActivationReason,providerId,propertyType,unitSubTypes,unitsOnTheFloor,legacyId,id,portal,unitFloor,parkingSpaces,updatedAt,address,suites,publicationType,externalId,bathrooms,usageTypes,totalAreas,advertiserId,advertiserContact,whatsappNumber,bedrooms,acceptExchange,pricingInfos,showPrice,resale,buildings,capacityLimit,status),account(id,name,logoUrl,licenseNumber,showAddress,legacyVivarealId,legacyZapId,minisite),medias,accountLink,link)),totalCount))',
+    # 'addressAccounts': '',
+    # 'addressType': 'city',
+    # 'addressCountry': '',
+    # 'cityWiseStreet': '1',
+    # 'developmentsSize': '3',
+    # 'superPremiumSize': '3',
+    # 'parentId': 'null',
+}
