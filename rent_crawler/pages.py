f8465a01e7be79162625aa7affe408b8f18c13ef
Morelatto <morelatto@pm.me>
2023-09-19 21:56:35 -0300

    refactor(pages): refactor page extraction

--- rent_crawler/pages.py
+++ rent_crawler/pages.py
@@ -1,12 +1,13 @@
 import hashlib
+import itertools
 import json
 from dataclasses import dataclass
+from datetime import datetime
 
-from web_poet import WebPage
-
-from rent_crawler.items import QuintoAndarProperty, QuintoAndarDetailsLoader, QuintoAndarPricesLoader, \
-    QuintoAndarTextDetails, VRZapProperty
+import pytz
+from rent_crawler.items import QuintoAndarProperty, QuintoAndarTextDetails, VRZapProperty
 from rent_crawler.providers import BodyJson
+from web_poet import WebPage
 
 
 @dataclass
@@ -18,7 +19,7 @@ class QuintoAndarPropertyHit:
     def address(self) -> dict:
         return {
             'street': self.source.get('address'),
-            'district': self.source.get('neighbourhood'),
+            'neighbourhood': self.source.get('neighbourhood'),
             'city': self.source.get('city'),
             'region': self.source.get('regionName')
         }
@@ -40,16 +41,11 @@ class QuintoAndarPropertyHit:
             'parking': self.source.get('parkingSpaces')
         }
 
-    @property
-    def media(self) -> dict:
-        images = ["/img/med/" + img for img in self.source.get('imageList')]
-        return dict(zip(self.source.get('imageCaptionList'), images))
-
     def to_item(self):
         address_hash = tuple(self.address.items())
         prices_hash = tuple(self.prices.items())
         serialized_data = json.dumps(address_hash + prices_hash).encode('utf-8')
-        return {'id': hashlib.md5(serialized_data).hexdigest()}
+        return {'key': hashlib.sha1(serialized_data).hexdigest()}
 
 
 class QuintoAndarListPage(WebPage):
@@ -76,7 +72,6 @@ class QuintoAndarListPage(WebPage):
                 address=property.address,
                 prices=property.prices,
                 details=property.details,
-                media=property.media,
                 url=f"/imovel/{property.id}"
             )
             yield item
@@ -90,77 +85,137 @@ class QuintoAndarPropertyPage(WebPage):
 
     @property
     def address(self):
-        district_city = self.css('div.MuiPaper-elevation1 small::text').get().split(', ')
+        address_data = self.json_data.get('address')
         return {
-            'street': self.css('div.MuiPaper-elevation1 h4::text').get(),
-            'district': district_city[0],
-            'city': district_city[1],
+            'street': address_data.get('street'),
+            'neighbourhood': address_data.get('neighborhood'),
+            'city': address_data.get('city'),
+            'cep': address_data.get('zipCode'),
+            'lat': address_data.get('lat'),
+            'lng': address_data.get('lng'),
         }
 
     @property
     def prices(self):
-        price_info_sel = "//p[contains(text(),'{}')]/../following-sibling::div/span/text()"
-        loader = QuintoAndarPricesLoader(selector=self.css('section[data-testid="house-pricebox"]'))
-        loader.add_xpath('rent', price_info_sel.format('Aluguel'))
-        loader.add_xpath('condominium', price_info_sel.format('Condomínio'))
-        loader.add_xpath('iptu', price_info_sel.format('IPTU'))
-        loader.add_xpath('insurance', price_info_sel.format('Seguro incêndio'))
-        loader.add_xpath('service_fee', price_info_sel.format('Taxa de serviço'))
-        loader.add_xpath('total', price_info_sel.format('Total'))
-        return loader.load_item()
+        data = self.json_data
+        return {
+            'rent': data.get('rentPrice'),
+            'condominium': data.get('condoPrice'),
+            'iptu': data.get('iptu'),
+            'insurance': data.get('homeProtection'),
+            'service_fee': data.get('tenantServiceFee'),
+            'total': data.get('totalCost'),
+        }
 
     @property
     def details(self):
-        main_info = self.css('div[data-testid="house-main-info"] p.CozyTypography:first-child::text').getall()
-        if main_info:
-            loader = QuintoAndarDetailsLoader()
-            loader.add_value('area', main_info[0])
-            loader.add_value('bedrooms', main_info[1])
-            loader.add_value('bathrooms', main_info[2])
-            loader.add_value('parking', main_info[3])
-            loader.add_value('floor', main_info[4])
-            loader.add_value('pet', main_info[5])
-            loader.add_value('furniture', main_info[6])
-            loader.add_value('subway', main_info[7])
-            return loader.load_item()
-        return ''
-
-    @property
-    def description(self):
-        texts = self.css("div.MuiGrid-root.MuiGrid-item p.CozyTypography::text").getall()
-        if len(texts) > 8:
-            return texts[8]
+        data = self.json_data
+        return {
+            'area': data.get('area'),
+            'bedrooms': data.get('bedrooms'),
+            'bathrooms': data.get('bathrooms'),
+            'parking': data.get('parkingSpaces'),
+            'floor': data.get('floor'),
+            'pet': data.get('acceptsPets'),
+            'furniture': data.get('hasFurniture'),
+            'suites': data.get('suites'),
+            'subway': data.get('isNearSubway'),
+        }
 
     @property
     def owner_description(self):
-        return self.xpath('//h4[text()="Descrição do proprietário"]/following-sibling::p/text()').get()
+        data = self.json_data
+        return data.get('remarks')
 
     @property
-    def items(self):
-        amenities_list = self.xpath('//div[@data-testid="amenities-list"]')
-        return {
-            "available": amenities_list.xpath(
-                '//h4[text()="Itens disponíveis"]/following-sibling::div//p/text()').getall(),
-            "unavailable": amenities_list.xpath(
-                '//h4[text()="Itens indisponíveis"]/following-sibling::div//p/text()').getall(),
-        }
+    def features(self):
+        return list(itertools.chain(
+            self.amenities,
+            self.comfort_commodities,
+            self.practicality_commodities,
+            self.installations,
+        ))
+
+    @property
+    def amenities(self):
+        data = self.json_data.get('amenities')
+        for amenity in data:
+            if amenity.get('value') == 'SIM':
+                yield amenity.get('key')
+
+    @property
+    def comfort_commodities(self):
+        data = self.json_data.get('comfortCommodities')
+        for commodity in data:
+            if commodity.get('value') == 'SIM':
+                yield commodity.get('key')
+
+    @property
+    def practicality_commodities(self):
+        data = self.json_data.get('practicalityCommodities')
+        for commodity in data:
+            if commodity.get('value') == 'SIM':
+                yield commodity.get('key')
+
+    @property
+    def installations(self):
+        data = self.json_data.get('installations')
+        for installation in data:
+            if installation.get('value') == 'SIM':
+                yield installation.get('key')
 
     @property
     def text_details(self):
         return QuintoAndarTextDetails(
             name=self.name,
-            description=self.description,
+            # description=self.description,
             owner_description=self.owner_description,
-            items=self.items,
+            features=self.features,
+            construction_year=self.json_data.get('constructionYear'),
+            publication_date=self.publication_date
         )
 
     @property
     def media(self):
-        return {"images": self.css('div.swiper-slide img::attr(data-src)').getall()}
+        data = self.json_data
+        return {
+            'images': list(self.images),
+            'video': data.get('video')
+        }
+
+    @property
+    def images(self, img_url='https://www.quintoandar.com.br/img/xxl/'):
+        data = self.json_data
+        for photo in data['photos']:
+            subtitle = photo.get('subtitle')
+            if subtitle is not None:
+                yield {subtitle: img_url + photo['url']}
 
     @property
     def publication_date(self):
-        return self.css('small[data-testid="publication_date"]::text').get('')
+        def parse_date(input_date, date_fmt='%Y-%m-%dT%H:%M:%S.%f%z', tz='America/Sao_Paulo'):
+            input_datetime = datetime.strptime(input_date, date_fmt)
+            input_datetime = input_datetime.replace(tzinfo=pytz.utc)
+            result_datetime = input_datetime.astimezone(pytz.timezone(tz))
+            return result_datetime.isoformat()
+
+        data = self.json_data
+        listings = data.get('listings')
+        if listings:
+            return {
+                'first': parse_date(listings[0].get('firstPublicationDate')),
+                'last': parse_date(listings[0].get('lastPublicationDate')),
+            }
+
+    @property
+    def json_data(self):
+        next_data = self.css('#__NEXT_DATA__::text').get()
+        json_data = json.loads(next_data)
+        props_data = json_data.get('props', {})
+        page_props = props_data.get('pageProps', {})
+        initial_state = page_props.get('initialState', {})
+        house = initial_state.get('house', {})
+        return house.get('houseInfo')
 
     def to_item(self):
         item = QuintoAndarProperty(
@@ -171,7 +226,6 @@ class QuintoAndarPropertyPage(WebPage):
             text_details=self.text_details,
             media=self.media,
             url=self.url,
-            publication_date=self.publication_date
         )
         return item
 
@@ -191,7 +245,7 @@ class VRZapPropertyHit:
         address = self.listing.get('address')
         return {
             'street': [address.get('street'), address.get('streetNumber')],
-            'district': address.get('neighbourhood'),
+            'neighbourhood': address.get('neighbourhood'),
             'city': address.get('city'),
             'complement': address.get('complement'),
             'cep': address.get('zipCode'),
@@ -225,7 +279,7 @@ class VRZapPropertyHit:
     def text_details(self) -> dict:
         return {
             'description': self.listing.get('description'),
-            'characteristics': self.listing.get('amenities'),
+            'features': self.listing.get('amenities'),
             'title': self.listing.get('title'),
             'contact': self.listing.get('advertiserContact').get('phones'),
             'type': self.listing.get('unitTypes')
